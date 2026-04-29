bbd4e59c5eee985e0fa0902afea383403f539a59
girish4600 <kelgane.girish@gmail.com>
2026-04-29 12:39:29 +0530

    ECOM-5 minor changes in kafka producers.

--- payment-service/src/main/java/com/gsk/payment/service/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/gsk/payment/service/PaymentServiceImpl.java
@@ -1,6 +1,9 @@
 package com.gsk.payment.service;
 
 import com.gsk.payment.conversion.PaymentMapper;
+import com.gsk.payment.kafka.NotificationProducer;
+import com.gsk.payment.kafka.model.PaymentNotificationRequest;
+import com.gsk.payment.model.PaymentMethod;
 import com.gsk.payment.model.PaymentRequest;
 import com.gsk.payment.model.PaymentResponse;
 import com.gsk.payment.repository.PaymentRepository;
@@ -19,9 +22,22 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     private PaymentMapper mapper;
 
+    @Autowired
+    private NotificationProducer notificationProducer;
+
     @Override
     public @Nullable Integer createPayment(PaymentRequest paymentRequest) {
         var payment = paymentRepository.save(mapper.dtoToEntity(paymentRequest));
+        notificationProducer.sendNotification(
+               new PaymentNotificationRequest(
+                       paymentRequest.orderReference(),
+                       paymentRequest.amount(),
+                        paymentRequest.paymentMethod(),
+                        paymentRequest.customer().firstName(),
+                        paymentRequest.customer().lastName(),
+                        paymentRequest.customer().email()
+                )
+        );
         return payment.getId();
     }
 
