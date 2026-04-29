2d8ffce793700bf1e27278f0d1036a6801923fba
girish4600 <kelgane.girish@gmail.com>
2026-04-29 10:45:10 +0530

    ECOM-5 make service compatible with kafka producer and docker

--- order-service/src/main/java/org/gsk/order/controller/OrderController.java
+++ order-service/src/main/java/org/gsk/order/controller/OrderController.java
@@ -2,8 +2,6 @@ package org.gsk.order.controller;
 
 import jakarta.validation.Valid;
 import org.gsk.order.model.OrderRequest;
-import org.gsk.order.orderline.model.OrderEvent;
-import org.gsk.order.orderline.service.OrderProducer;
 import org.gsk.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +14,9 @@ public class OrderController {
     @Autowired
     private OrderService orderServiceImpl;
 
-    @Autowired
-    private OrderProducer orderProducer;
-
     @PostMapping
     public ResponseEntity<String> placeOrder(@RequestBody @Valid OrderRequest orderRequest) {
         var response = orderServiceImpl.placeOrder(orderRequest);
-//        OrderEvent event = new OrderEvent(response, "gsk@gmail.com", "CONFIRMED");
-//        orderProducer.sendOrderEvent(event);
-
         return ResponseEntity.ok(response);
     }
 
