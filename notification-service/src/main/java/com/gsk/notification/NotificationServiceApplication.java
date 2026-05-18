7b0251cad8a9eb2e30622f66873a209850666b52
girish4600 <kelgane.girish@gmail.com>
2026-05-18 23:29:36 +0530

    ECOM-10 confirm notification over email.

--- notification-service/src/main/java/com/gsk/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/gsk/notification/NotificationServiceApplication.java
@@ -5,11 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class
-NotificationServiceApplication {
+@EnableAsync
+public class NotificationServiceApplication {
 
 
 	@Value("${spring.data.mongodb.uri}")
